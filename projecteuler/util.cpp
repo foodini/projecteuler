74ce576631145cd0957046b9c6a9948ab5e3d4f4
Ron Barry <foodini@gmail.com>
2023-07-24 17:07:45 -0700

    Add mins/maxes for int types. Add print_s128

--- projecteuler/util.cpp
+++ projecteuler/util.cpp
@@ -2,16 +2,53 @@
 
 #include "util.h"
 
+const u8 u8_min = 0x00;
+const u8 u8_max = 0xff;
+const s8 s8_min = -128;
+const s8 s8_max = 127;
+const u16 u16_min = 0x0000;
+const u16 u16_max = 0xffff;
+const s16 s16_min = 0x8000;
+const s16 s16_max = 0x7fff;
+const u32 u32_min = 0x00000000;
+const u32 u32_max = 0xffffffff;
+const s32 s32_min = 0x80000000;
+const s32 s32_max = 0x7fffffff;
+const u64 u64_min = 0x0000000000000000UL;
+const u64 u64_max = 0xffffffffffffffffUL;
+const s64 s64_min = 0x8000000000000000UL;
+const s64 s64_max = 0x7fffffffffffffffUL;
+const u128 u128_min = 0;
+const u128 u128_max = ~((s128)1);
+const s128 s128_min = ((s128)1) << 127;
+const s128 s128_max = (((s128)s64_max) << 64) | (s128)u64_max;
+
 void print_u128(u128 u, char end) {
+  char buf[80];
+  char * cur = buf + 79;
+  *cur-- = '\0';
+  *cur-- = end;
+  do {
+    *cur-- = '0' + (u % 10);
+    u /= 10;
+  } while(u);
+  printf("%s", cur + 1);
+}
+
+void print_s128(s128 s, char end) {
+  if(s<0) {
+    printf("-");
+    s = -s;
+  }
+  print_u128(s, end);
+}
+
+void print_x128(u128 u, char end) {
   u64 mask = -1ULL;
   u64 least = u & mask;
   u64 most = u >> 64;
-  if(most)
-    printf("%lu%lu", most, least);
-  else
-    printf("%lu", least);
-  if(end)
-    printf("%c", end);
+  printf("0x%08lx%08lx", most, least);
+  printf("%c", end);
 }
 
 double now() {
--- projecteuler/util.h
+++ projecteuler/util.h
@@ -1,16 +1,42 @@
+#ifndef _UTIL_H
+#define _UTIL_H
 #include <sys/time.h>
 
 typedef unsigned char u8;
+extern const u8 u8_min;
+extern const u8 u8_max;
 typedef char s8;
+extern const s8 s8_min;
+extern const s8 s8_max;
 typedef unsigned short u16;
+extern const u16 u16_min;
+extern const u16 u16_max;
 typedef short s16;
+extern const s16 s16_min;
+extern const s16 s16_max;
 typedef unsigned int u32;
+extern const u32 u32_min;
+extern const u32 u32_max;
 typedef int s32;
+extern const s32 s32_min;
+extern const s32 s32_max;
 typedef unsigned long u64;
+extern const u64 u64_min;
+extern const u64 u64_max;
 typedef long s64;
+extern const s64 s64_min;
+extern const s64 s64_max;
 typedef __uint128_t u128;
+extern const u128 u128_min;
+extern const u128 u128_max;
 typedef __int128_t s128;
+extern const s128 s128_min;
+extern const s128 s128_max;
 
 void print_u128(u128 u, char end='\0');
+void print_x128(u128 u, char end='\0');
+void print_s128(s128 u, char end='\0');
 double now();
 bool progress_timer();
+
+#endif // __UTIL_H
